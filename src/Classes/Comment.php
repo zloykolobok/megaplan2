3e264732b4e715d272dd49d9ca806bcfb4efa1ae
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2019-04-14 12:33:08 +0300

    added list comments

--- src/Classes/Comment.php
+++ src/Classes/Comment.php
@@ -36,4 +36,72 @@ class Comment extends Megaplan
 
         return $raw;
     }
+
+    /**
+     * Список комментариев по задаче/проекту/клиенту/сделке/делу/переписке
+     *
+     *
+     * @param $subjectType - string
+     *      task (задача), project (проект), contractor (клиент),
+     *      deal (сделка), item(дело), discuss(переписка) - Тип комментируемого объекта
+     * @param $subjectId - integer - ID комментируемого объекта
+     * @param $timeUpdated - string	Дата/время в одном из форматов ISO 8601 -
+     *      Возвращать только те объекты, которые были изменены после указанный даты
+     * @param $order - asc (по возрастанию), desc (по убыванию) -
+     *      Направление сортировки по дате (по умолчанию asc)
+     * @param $textHtml - bool - Возвращать ли комментарий в Html формате (по умолчанию false)
+     * @param $unreadOnly - bool - Возвращает только непрочитанные комментарии если true, по умолчанию false
+     * @param $limit - integer - Сколько выбрать комментариев (LIMIT)
+     * @param $offset - integer - Начиная с какого выбирать комментарии (OFFSET)
+     * @param bool $droppedOnly - true	Возвращать только удаленные комментарии
+     * @return mixed
+     * @throws \Zloykolobok\Megaplan2\Exception\ConnectionException
+     */
+    public function list(
+        $subjectType,
+        $subjectId,
+        $timeUpdated,
+        $order,
+        $textHtml,
+        $unreadOnly,
+        $limit,
+        $offset,
+        $droppedOnly = false
+    )
+    {
+        $this->auth();
+
+        $params = [];
+        $params['SubjectType'] = $subjectType;
+        $params['SubjectId'] = $subjectId;
+        if(!is_null($timeUpdated)) {
+            $params['TimeUpdated'] = $timeUpdated;
+        }
+        if(!is_null($order)){
+            $params['Order'] = $order;
+        }
+
+        if(!is_null($textHtml)){
+            $params['TextHtml'] = $textHtml;
+        }
+
+        if(!is_null($unreadOnly)){
+            $params['UnreadOnly'] = $unreadOnly;
+        }
+
+        if(!is_null($limit)){
+            $params['Limit'] = $limit;
+        }
+
+        if(!is_null($offset)){
+            $params['Offset'] = $offset;
+        }
+
+        $params['DroppedOnly'] = $droppedOnly;
+
+        $raw = $this->req->post('/BumsCommonApiV01/Comment/list.api',$params);
+        $raw = json_decode($raw);
+
+        return $raw;
+    }
 }
